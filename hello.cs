93644e80a935741d5e57a0f2831ca466230ebe15
Kamil <kamilhidayat.890@gmail.com>
2021-04-25 12:56:57 +0700

    Add files via upload

---
+++ hello.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace MyApplication
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      string first;
+      string last;
+      string name = first + last;
+      Console.ReadLine("First Name");
+      Console.WriteLine("First Name"+ first);
+      Console.WriteLine(name);
+      Console.ReadKey("Click any button");
+    }
+  }
+}
